9c14d66176bcf7881b32b854ebdee07d14df2715
Carlos Marques <carrlosmr@gmail.com>
2024-10-27 18:39:08 -0300

    feat: verificando se o estado existe antes de criar uma cidade

--- src/main/java/com/delivery/api/application/use_cases/city/CreateCity.java
+++ src/main/java/com/delivery/api/application/use_cases/city/CreateCity.java
@@ -1,6 +1,7 @@
 package com.delivery.api.application.use_cases.city;
 
 import com.delivery.api.application.service.CityService;
+import com.delivery.api.application.service.StateService;
 import com.delivery.api.domain.entity.City;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,15 +10,19 @@ import org.springframework.stereotype.Component;
 public class CreateCity {
 
     private final CityService cityService;
+    private final StateService stateService;
 
     @Autowired
     public CreateCity(
-            CityService cityService
+            CityService cityService,
+            StateService stateService
     ){
             this.cityService = cityService;
+            this.stateService = stateService;
     }
 
     public City execute(City city){
+        this.stateService.findState(city.getState().getId());
         return this.cityService.createCity(city);
     }
 
