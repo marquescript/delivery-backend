f95274092abd47cfd1df5b7691a0ad64f5f9754b
Carlos Marques <carrlosmr@gmail.com>
2024-10-27 18:23:24 -0300

    feat: adverificando se a cidade no endereço existe

--- src/main/java/com/delivery/api/application/use_cases/address/CreateAddress.java
+++ src/main/java/com/delivery/api/application/use_cases/address/CreateAddress.java
@@ -1,21 +1,28 @@
 package com.delivery.api.application.use_cases.address;
 
 import com.delivery.api.application.service.AddressService;
+import com.delivery.api.application.service.CityService;
 import com.delivery.api.domain.entity.Address;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CreateAddress {
 
     private final AddressService addressService;
+    private final CityService cityService;
 
+    @Autowired
     public CreateAddress(
-            AddressService addressService
+            AddressService addressService,
+            CityService cityService
     ){
             this.addressService = addressService;
+            this.cityService = cityService;
     }
 
     public Address execute(Address address){
+        this.cityService.findCity(address.getCity().getId());
         return this.addressService.createAddress(address);
     }
 
