f72c36f90387dffcde1450f0bd8c36e9f675ff5e
Carlos Marques <carrlosmr@gmail.com>
2024-10-27 19:07:46 -0300

    feat: adicionando metodo do controller para criar endereço

--- src/main/java/com/delivery/api/infra/http/controller/AddressController.java
+++ src/main/java/com/delivery/api/infra/http/controller/AddressController.java
@@ -1,5 +1,12 @@
 package com.delivery.api.infra.http.controller;
 
+import com.delivery.api.application.use_cases._factory.AddressUseCase;
+import com.delivery.api.domain.entity.Address;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/address")
 public class AddressController {
 
+    private final AddressUseCase addressUseCase;
 
+    @Autowired
+    public AddressController(
+            AddressUseCase addressUseCase
+    ){
+            this.addressUseCase = addressUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<Address> createAddress(@RequestBody Address address){
+        address = this.addressUseCase.createAddressUseCase(address);
+        return ResponseEntity.status(HttpStatus.CREATED).body(address);
+    }
 
 }
