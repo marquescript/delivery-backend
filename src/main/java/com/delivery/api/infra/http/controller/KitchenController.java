e93f20c1bdcea2c115c6906c13791a1506faa90e
Carlos Marques <carrlosmr@gmail.com>
2024-10-29 21:04:12 -0300

    feat: adicionando metodos no controller de cozinha

---
+++ src/main/java/com/delivery/api/infra/http/controller/KitchenController.java
@@ -0,0 +1,56 @@
+package com.delivery.api.infra.http.controller;
+
+import com.delivery.api.application.use_cases._factory.KitchenUseCase;
+import com.delivery.api.domain.entity.Kitchen;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/kitchen")
+public class KitchenController {
+
+    private final KitchenUseCase kitchenUseCase;
+
+    @Autowired
+    public KitchenController(
+            KitchenUseCase kitchenUseCase
+    ){
+            this.kitchenUseCase = kitchenUseCase;
+    }
+
+    @GetMapping("/{kitchenId}")
+    public ResponseEntity<Kitchen> findKitchen(@PathVariable Long kitchenId){
+        Kitchen kitchen = this.kitchenUseCase.findKitchenUseCase(kitchenId);
+        return ResponseEntity.status(HttpStatus.OK).body(kitchen);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Kitchen>> findAllKitchens(@RequestParam(value = "page", defaultValue = "0") int page,
+                                                         @RequestParam(value = "size", defaultValue = "10") int size){
+        List<Kitchen> kitchens = this.kitchenUseCase.findAllKitchensUseCase(page, size);
+        return ResponseEntity.status(HttpStatus.OK).body(kitchens);
+    }
+
+    @PostMapping
+    public ResponseEntity<Kitchen> saveKitchen(@RequestBody Kitchen kitchen){
+        Kitchen savedKitchen = this.kitchenUseCase.createKitchenUseCase(kitchen);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedKitchen);
+    }
+
+    @PutMapping("/{kitchenId}")
+    public ResponseEntity<Kitchen> updateKitchen(@PathVariable Long kitchenId, @RequestBody Kitchen kitchen){
+        Kitchen updatedKitchen = this.kitchenUseCase.updateKitchenUseCase(kitchenId, kitchen);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedKitchen);
+    }
+
+    @DeleteMapping("/{kitchenId}")
+    public ResponseEntity<Void> deleteKitchen(@PathVariable Long kitchenId){
+        this.kitchenUseCase.deleteKitchenUseCase(kitchenId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+}
