6030234686fba972f1f2dd3059d9533105d3af93
Carlos Marques <carrlosmr@gmail.com>
2024-10-30 21:01:45 -0300

    feat: adicionando metodos no controller de produto

---
+++ src/main/java/com/delivery/api/application/use_cases/_factory/ProductUseCase.java
@@ -0,0 +1,62 @@
+package com.delivery.api.application.use_cases._factory;
+
+import com.delivery.api.application.use_cases.product.*;
+import com.delivery.api.domain.entity.Product;
+import com.delivery.api.infra.http.dto.ProductRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ProductUseCase {
+
+    private final CreateProduct createProduct;
+    private final DeleteProduct deleteProduct;
+    private final FindProductByRestaurant findProductByRestaurant;
+    private final FindProductsByRestaurant findProductsByRestaurant;
+    private final FindProductsByRestaurantAndName findProductsByRestaurantAndName;
+    private final FindProductsByRestaurantAndCategory findProductsByRestaurantAndCategory;
+
+    @Autowired
+    public ProductUseCase(
+            CreateProduct createProduct,
+            DeleteProduct deleteProduct,
+            FindProductByRestaurant findProductByRestaurant,
+            FindProductsByRestaurant findProductsByRestaurant,
+            FindProductsByRestaurantAndName findProductsByRestaurantAndName,
+            FindProductsByRestaurantAndCategory findProductsByRestaurantAndCategory
+    ){
+            this.createProduct = createProduct;
+            this.deleteProduct = deleteProduct;
+            this.findProductByRestaurant = findProductByRestaurant;
+            this.findProductsByRestaurant = findProductsByRestaurant;
+            this.findProductsByRestaurantAndName = findProductsByRestaurantAndName;
+            this.findProductsByRestaurantAndCategory = findProductsByRestaurantAndCategory;
+    }
+
+    public Product createProductUseCase(ProductRequest productRequest){
+        return this.createProduct.execute(productRequest);
+    }
+
+    public void deleteProductUseCase(Long restaurantId, Long productId){
+        this.deleteProduct.execute(restaurantId, productId);
+    }
+
+    public Product findProductByRestaurantUseCase(Long restaurantId, Long productId){
+        return this.findProductByRestaurant.execute(restaurantId, productId);
+    }
+
+    public List<Product> findProductsByRestaurantUseCase(Long restaurantId, int page, int size){
+        return this.findProductsByRestaurant.execute(restaurantId, page, size);
+    }
+
+    public List<Product> findProductsByRestaurantAndNameUseCase(Long restaurantId, String name, int page, int size){
+        return this.findProductsByRestaurantAndName.execute(restaurantId, name, page, size);
+    }
+
+    public List<Product> findProductsByRestaurantAndCategoryUseCase(Long restaurantId, Long categoryId, int page, int size){
+        return this.findProductsByRestaurantAndCategory.execute(restaurantId, categoryId, page, size);
+    }
+
+}
---
+++ src/main/java/com/delivery/api/infra/http/controller/ProductController.java
@@ -0,0 +1,58 @@
+package com.delivery.api.infra.http.controller;
+
+import com.delivery.api.application.use_cases._factory.ProductUseCase;
+import com.delivery.api.domain.entity.Product;
+import com.delivery.api.infra.http.dto.ProductRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/restaurant")
+public class ProductController {
+
+    private final ProductUseCase productUseCase;
+
+    @Autowired
+    public ProductController(
+            ProductUseCase productUseCase
+    ){
+            this.productUseCase = productUseCase;
+    }
+
+    @GetMapping("/{restaurantId}/product/{productId}")
+    public ResponseEntity<Product> findProductByRestaurant(Long restaurantId, Long productId){
+        Product product = this.productUseCase.findProductByRestaurantUseCase(restaurantId, productId);
+        return ResponseEntity.status(HttpStatus.OK).body(product);
+    }
+
+    @GetMapping("/{restaurantId}/product")
+    public ResponseEntity<List<Product>> findProductsByRestaurant(@PathVariable Long restaurantId, @RequestParam(value = "page", defaultValue = "0") int page,
+                                                                  @RequestParam(value = "page", defaultValue = "0") int size){
+        List<Product> products = this.productUseCase.findProductsByRestaurantUseCase(restaurantId, page, size);
+        return ResponseEntity.status(HttpStatus.OK).body(products);
+    }
+
+    @GetMapping("/{restaurantId}/product/seach")
+    public ResponseEntity<List<Product>> findProductsByRestaurantAndName(@PathVariable Long restaurantId, @RequestParam(value = "product-name") String productName, @RequestParam(value = "page", defaultValue = "0") int page,
+                                                                  @RequestParam(value = "page", defaultValue = "0") int size){
+        List<Product> products = this.productUseCase.findProductsByRestaurantAndNameUseCase(restaurantId, productName, page, size);
+        return ResponseEntity.status(HttpStatus.OK).body(products);
+    }
+
+    @PostMapping("/{restaurantId}/product")
+    public ResponseEntity<Product> createProduct(@RequestBody ProductRequest productRequest){
+        Product product = this.productUseCase.createProductUseCase(productRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+
+    @DeleteMapping("/{restaurantId}/product/{productId}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long restaurantId, @PathVariable Long productId){
+        this.productUseCase.deleteProductUseCase(restaurantId, productId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+}
